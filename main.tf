88df1a97c681b7d86221dad0d55fc41a6224a22f
nishantparsai <nishantparsai@gmail.com>
2025-03-16 12:44:15 +0530

    update

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_resource_group" "rg-dhondhu" {
+  name     = "rg-dhondhu"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "rg-tommy" {
+  name     = "rg-tommy"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "rg-rahul" {
+  name     = "rg-rahul"
+  location = "CentralIndia"
+}
+
+resource "azurerm_resource_group" "rg-234erw" {
+  name     = "rg-sfdgsdfg"
+  location = "CentralIndia"
+}
